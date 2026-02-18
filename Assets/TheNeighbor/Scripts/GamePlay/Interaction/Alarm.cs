a1670d70d6de9170677cc06a0b6fa57954b14300
Anton <adobonii@qualium-systems.com>
2026-02-18 23:52:05 +0200

    Bedroom wip

---
+++ Assets/TheNeighbor/Scripts/GamePlay/Interaction/Alarm.cs
@@ -0,0 +1,39 @@
+using System;
+using Trellcko.Gameplay.Interactable;
+using Trellcko.Gameplay.QuestLogic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour, IInteractable
+{
+    [field: SerializeField] public InteractableOutline InteractableOutline { get; private set; }
+    
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _alarm;
+    [SerializeField] private AudioClip _turnOffAlarm;
+
+    public event Action Interacted;
+    public bool IsInteractable { get; private set; }
+
+    private void Start()
+    {
+        Activate();
+    }
+
+    public void Activate()
+    {
+        IsInteractable = true;
+        _audioSource.clip = _alarm;
+        _audioSource.loop = true;
+        _audioSource.Play();
+    }
+    
+    public bool TryInteract(out QuestItem getItem, QuestItem neededItem)
+    {
+        getItem = neededItem;
+        if (!IsInteractable) return false;
+        _audioSource.clip = _turnOffAlarm;
+        _audioSource.loop = false;
+        _audioSource.Play();
+        return true;
+    }
+}
